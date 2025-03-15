3f3ee7d5bf29c6b0884a1295feea6960ba0e6119
1526414 <1526414@uab.cat>
2025-03-15 18:51:44 +0100

    first

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink), //no cambia nada aun
+      ),
+      home: const NotesPage(),
+    );
+  }
+}
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  final List<String> _notes = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addNote() {
+    if (_controller.text.isNotEmpty) { //si la nota tiene texto, la añade
+      setState(() {
+        _notes.add(_controller.text);
+        _controller.clear();
+      });
+    }
+  }
+
+  void _removeNote(int index) {
+    setState(() {
+      _notes.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Escribe una nota',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: _addNote,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _notes.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_notes[index]),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => _removeNote(index),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
